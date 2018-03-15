76f242ea9231532d1c0cfdc29bc4fd3f760c985b
Alex Devero <deveroalex@gmail.com>
2018-03-15 09:01:35 +0100

    Add webpack configs

---
+++ webpack.build.config.js
@@ -0,0 +1,53 @@
+const webpack = require('webpack')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const BabiliPlugin = require('babili-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+// Any directories you will be adding code/files into, need to be added to this array so webpack will pick them up
+const defaultInclude = path.resolve(__dirname, 'src')
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [{ loader: 'css-loader', loader: 'postcss-loader'}]
+        }),
+        include: defaultInclude
+      },
+      {
+        test: /\.jsx?$/,
+        use: [{ loader: 'babel-loader' }],
+        include: defaultInclude
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/,
+        use: [{ loader: 'file-loader?name=img/[name]__[hash:base64:5].[ext]' }],
+        include: defaultInclude
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        use: [{ loader: 'file-loader?name=font/[name]__[hash:base64:5].[ext]' }],
+        include: defaultInclude
+      }
+    ]
+  },
+  target: 'electron-renderer',
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new ExtractTextPlugin('bundle.css'),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new BabiliPlugin()
+  ],
+  stats: {
+    colors: true,
+    children: false,
+    chunks: false,
+    modules: false
+  }
+}
