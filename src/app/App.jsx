b735083f38b4af9b2fb3e2ef5031570c8c0ccb18
Alex Devero <deveroalex@gmail.com>
2018-03-15 09:28:03 +0100

    Add source code

---
+++ src/app/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Electron - React - MobX Boilerplate</h1>
+
+      <p>Let's begin!</p>
+    </div>
+  )
+}
+
+export default App
